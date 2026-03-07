bebb71538b272ff520daedb245e019363a136bc6
Herosoftware01 <herosoftware@herofashion.com>
2026-03-07 14:54:45 +0530

    1234

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ function Dashboard() {
     <div style={{ display: "flex" }}>
       {/* <Sidebar /> */}
       <div style={{ padding: "20px" }}>
-        <h2>Welcome HeroFashion</h2>
+        <h2>Welcome HeroFashion User</h2>
       </div>
     </div>
   );

20f9bf5ad5aeaa379c45f6e04069c2b908b4756e
Herosoftware01 <herosoftware@herofashion.com>
2026-03-31 18:32:40 +0530

    latest123

--- src/components/syncfushion/order/sync_iframe.jsx
+++ src/components/syncfushion/order/sync_iframe.jsx
@@ -2,7 +2,7 @@ function MyPage() {
   return (
     <div style={{ margin: 0, padding: 0 }}>
       <iframe
-        src="https://api.herofashion.com/bi/site/site2/dashboards/a9f3eb35-2d95-43cb-bcab-cfbcbd184154/Hero%20Order/NewOrder?isembed=true&views=false&embed_dashboard_views_edit=false&hide_header=false&hide_metrics=false&hide_dashboard_edit=true&hide_language_dropdown=false&dashboard_comments=false&widget_comments=false&enable_ai_assistant=false"
+        src="https://api.herofashion.com/bi/site/site2/dashboards/a9f3eb35-2d95-43cb-bcab-cfbcbd184154/Hero%20Order/NewOrder"
         title="Dashboard"
         style={{
           width: "100vw",
